56db012f88784cb8e9b75957d564a690375f7728
Saimon Michelson <smichelson@saimons-mbp.home>
2021-10-25 23:31:11 -0400

    support list audit trail events

--- papi/constructs/audit.py
+++ papi/constructs/audit.py
@@ -6,8 +6,25 @@ class Audit(Construct):
     Class for accessing a workspace audit log
     """
 
-    # use custom get function with additional headers
-    # https://api.segmentapis.com/docs/admin/audit-trail/#list-audit-events
-    def all(self, start_time, end_time, resource_id, resource_type):
-        print(self, start_time, end_time, resource_id, resource_type)
-        # return self._segment.iterator('/audit-events', 'events')
+    def all(self, start_time=None, end_time=None, resource_id=None, resource_type=None):
+        """
+        List audit trail events
+
+        :param datetime.datetime,optional start_time: Filter response to events that happened after this time
+        :param datetime.datetime,optional end_time: Filter response to events that happened before this time, defaults to the current time.
+        :param str resource_id: Filter response to events that affect a specific resource
+        :param str resource_type: Filter response to events that affect a specific type
+        :returns: List of audit trail events
+        :rtype: papi.common.common.Object
+        """
+        # https://api.segmentapis.com/docs/admin/audit-trail/#list-audit-events
+        params = {}
+        if start_time:
+            params['startTime'] = start_time.isoformat()
+        if end_time:
+            params['endTime'] = end_time.isoformat()
+        if resource_id:
+            params['resourceId'] = resource_id
+        if resource_type:
+            params['resourceType'] = resource_type
+        return self._segment.iterator('/audit-trails', params).events
