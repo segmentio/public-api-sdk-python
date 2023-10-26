4fa67af29f01e912fe7ff7bf9be95fb1aa0e6725
Arun Lalam <arunlalam@gmail.com>
2023-10-26 14:05:11 -0700

    open source release

---
+++ segment_public_api/api/__init__.py
@@ -0,0 +1,30 @@
+# flake8: noqa
+
+# import apis into api package
+from segment_public_api.api.api_calls_api import APICallsApi
+from segment_public_api.api.audiences_api import AudiencesApi
+from segment_public_api.api.audit_trail_api import AuditTrailApi
+from segment_public_api.api.catalog_api import CatalogApi
+from segment_public_api.api.computed_traits_api import ComputedTraitsApi
+from segment_public_api.api.deletion_and_suppression_api import DeletionAndSuppressionApi
+from segment_public_api.api.destination_filters_api import DestinationFiltersApi
+from segment_public_api.api.destinations_api import DestinationsApi
+from segment_public_api.api.edge_functions_api import EdgeFunctionsApi
+from segment_public_api.api.events_api import EventsApi
+from segment_public_api.api.functions_api import FunctionsApi
+from segment_public_api.api.iam_groups_api import IAMGroupsApi
+from segment_public_api.api.iam_roles_api import IAMRolesApi
+from segment_public_api.api.iam_users_api import IAMUsersApi
+from segment_public_api.api.labels_api import LabelsApi
+from segment_public_api.api.monthly_tracked_users_api import MonthlyTrackedUsersApi
+from segment_public_api.api.profiles_sync_api import ProfilesSyncApi
+from segment_public_api.api.reverse_etl_api import ReverseETLApi
+from segment_public_api.api.selective_sync_api import SelectiveSyncApi
+from segment_public_api.api.sources_api import SourcesApi
+from segment_public_api.api.spaces_api import SpacesApi
+from segment_public_api.api.testing_api import TestingApi
+from segment_public_api.api.tracking_plans_api import TrackingPlansApi
+from segment_public_api.api.transformations_api import TransformationsApi
+from segment_public_api.api.warehouses_api import WarehousesApi
+from segment_public_api.api.workspaces_api import WorkspacesApi
+
