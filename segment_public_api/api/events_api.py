b16b556fa44ab35596a2e7ff59fad77bbd600629
APIs and Common Services team <team-apics@twilio.com>
2023-11-10 03:10:49 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@42594c71.

--- segment_public_api/api/events_api.py
+++ segment_public_api/api/events_api.py
@@ -22,7 +22,7 @@ from pydantic import validate_arguments, ValidationError
 from typing_extensions import Annotated
 from pydantic import Field, StrictStr, conlist
 
-from typing import List, Optional
+from typing import Optional
 
 from segment_public_api.models.get_events_volume_from_workspace200_response import GetEventsVolumeFromWorkspace200Response
 from segment_public_api.models.pagination_input import PaginationInput
@@ -48,7 +48,7 @@ class EventsApi:
         self.api_client = api_client
 
     @validate_arguments
-    def get_events_volume_from_workspace(self, granularity : Annotated[StrictStr, Field(..., description="The size of each bucket in the requested window.  This parameter exists in v1.")], start_time : Annotated[StrictStr, Field(..., description="The ISO8601 formatted timestamp that corresponds to the beginning of the requested time frame, inclusive.  This parameter exists in v1.")], end_time : Annotated[StrictStr, Field(..., description="The ISO8601 formatted timestamp that corresponds to the end of the requested time frame, noninclusive. Segment recommends that you lag queries 1 minute behind clock time to reduce the risk for latency to impact the counts.  This parameter exists in v1.")], group_by : Annotated[Optional[conlist(conlist(StrictStr))], Field(description="A comma-delimited list of strings that represents the dimensions to group the result by. The options are: `eventName`, `eventType` and `source`.  This parameter exists in v1.")] = None, source_id : Annotated[Optional[conlist(conlist(StrictStr))], Field(description="A list of strings which filters the results to the given SourceIds.  This parameter exists in v1.")] = None, event_name : Annotated[Optional[conlist(conlist(StrictStr))], Field(description="A list of strings which filters the results to the given EventNames.  This parameter exists in v1.")] = None, event_type : Annotated[Optional[conlist(conlist(StrictStr))], Field(description="A list of strings which filters the results to the given EventTypes.  This parameter exists in v1.")] = None, app_version : Annotated[Optional[conlist(conlist(StrictStr))], Field(description="A list of strings which filters the results to the given AppVersions.  This parameter exists in v1.")] = None, pagination : Annotated[Optional[PaginationInput], Field(description="Pagination input for event volume by Workspace.  This parameter exists in v1.")] = None, **kwargs) -> GetEventsVolumeFromWorkspace200Response:  # noqa: E501
+    def get_events_volume_from_workspace(self, granularity : Annotated[StrictStr, Field(..., description="The size of each bucket in the requested window.  This parameter exists in v1.")], start_time : Annotated[StrictStr, Field(..., description="The ISO8601 formatted timestamp that corresponds to the beginning of the requested time frame, inclusive.  This parameter exists in v1.")], end_time : Annotated[StrictStr, Field(..., description="The ISO8601 formatted timestamp that corresponds to the end of the requested time frame, noninclusive. Segment recommends that you lag queries 1 minute behind clock time to reduce the risk for latency to impact the counts.  This parameter exists in v1.")], group_by : Annotated[Optional[conlist(StrictStr)], Field(description="A comma-delimited list of strings that represents the dimensions to group the result by. The options are: `eventName`, `eventType` and `source`.  This parameter exists in v1.")] = None, source_id : Annotated[Optional[conlist(StrictStr)], Field(description="A list of strings which filters the results to the given SourceIds.  This parameter exists in v1.")] = None, event_name : Annotated[Optional[conlist(StrictStr)], Field(description="A list of strings which filters the results to the given EventNames.  This parameter exists in v1.")] = None, event_type : Annotated[Optional[conlist(StrictStr)], Field(description="A list of strings which filters the results to the given EventTypes.  This parameter exists in v1.")] = None, app_version : Annotated[Optional[conlist(StrictStr)], Field(description="A list of strings which filters the results to the given AppVersions.  This parameter exists in v1.")] = None, pagination : Annotated[Optional[PaginationInput], Field(description="Pagination input for event volume by Workspace.  This parameter exists in v1.")] = None, **kwargs) -> GetEventsVolumeFromWorkspace200Response:  # noqa: E501
         """Get Events Volume from Workspace  # noqa: E501
 
         Enumerates the Workspace event volumes over time in minute increments.   The rate limit for this endpoint is 60 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information.  # noqa: E501
@@ -94,7 +94,7 @@ class EventsApi:
         return self.get_events_volume_from_workspace_with_http_info(granularity, start_time, end_time, group_by, source_id, event_name, event_type, app_version, pagination, **kwargs)  # noqa: E501
 
     @validate_arguments
-    def get_events_volume_from_workspace_with_http_info(self, granularity : Annotated[StrictStr, Field(..., description="The size of each bucket in the requested window.  This parameter exists in v1.")], start_time : Annotated[StrictStr, Field(..., description="The ISO8601 formatted timestamp that corresponds to the beginning of the requested time frame, inclusive.  This parameter exists in v1.")], end_time : Annotated[StrictStr, Field(..., description="The ISO8601 formatted timestamp that corresponds to the end of the requested time frame, noninclusive. Segment recommends that you lag queries 1 minute behind clock time to reduce the risk for latency to impact the counts.  This parameter exists in v1.")], group_by : Annotated[Optional[conlist(conlist(StrictStr))], Field(description="A comma-delimited list of strings that represents the dimensions to group the result by. The options are: `eventName`, `eventType` and `source`.  This parameter exists in v1.")] = None, source_id : Annotated[Optional[conlist(conlist(StrictStr))], Field(description="A list of strings which filters the results to the given SourceIds.  This parameter exists in v1.")] = None, event_name : Annotated[Optional[conlist(conlist(StrictStr))], Field(description="A list of strings which filters the results to the given EventNames.  This parameter exists in v1.")] = None, event_type : Annotated[Optional[conlist(conlist(StrictStr))], Field(description="A list of strings which filters the results to the given EventTypes.  This parameter exists in v1.")] = None, app_version : Annotated[Optional[conlist(conlist(StrictStr))], Field(description="A list of strings which filters the results to the given AppVersions.  This parameter exists in v1.")] = None, pagination : Annotated[Optional[PaginationInput], Field(description="Pagination input for event volume by Workspace.  This parameter exists in v1.")] = None, **kwargs) -> ApiResponse:  # noqa: E501
+    def get_events_volume_from_workspace_with_http_info(self, granularity : Annotated[StrictStr, Field(..., description="The size of each bucket in the requested window.  This parameter exists in v1.")], start_time : Annotated[StrictStr, Field(..., description="The ISO8601 formatted timestamp that corresponds to the beginning of the requested time frame, inclusive.  This parameter exists in v1.")], end_time : Annotated[StrictStr, Field(..., description="The ISO8601 formatted timestamp that corresponds to the end of the requested time frame, noninclusive. Segment recommends that you lag queries 1 minute behind clock time to reduce the risk for latency to impact the counts.  This parameter exists in v1.")], group_by : Annotated[Optional[conlist(StrictStr)], Field(description="A comma-delimited list of strings that represents the dimensions to group the result by. The options are: `eventName`, `eventType` and `source`.  This parameter exists in v1.")] = None, source_id : Annotated[Optional[conlist(StrictStr)], Field(description="A list of strings which filters the results to the given SourceIds.  This parameter exists in v1.")] = None, event_name : Annotated[Optional[conlist(StrictStr)], Field(description="A list of strings which filters the results to the given EventNames.  This parameter exists in v1.")] = None, event_type : Annotated[Optional[conlist(StrictStr)], Field(description="A list of strings which filters the results to the given EventTypes.  This parameter exists in v1.")] = None, app_version : Annotated[Optional[conlist(StrictStr)], Field(description="A list of strings which filters the results to the given AppVersions.  This parameter exists in v1.")] = None, pagination : Annotated[Optional[PaginationInput], Field(description="Pagination input for event volume by Workspace.  This parameter exists in v1.")] = None, **kwargs) -> ApiResponse:  # noqa: E501
         """Get Events Volume from Workspace  # noqa: E501
 
         Enumerates the Workspace event volumes over time in minute increments.   The rate limit for this endpoint is 60 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information.  # noqa: E501
@@ -200,23 +200,23 @@ class EventsApi:
 
         if _params.get('group_by') is not None:  # noqa: E501
             _query_params.append(('groupBy', _params['group_by']))
-            _collection_formats['groupBy'] = 'csv'
+            _collection_formats['groupBy'] = 'multi'
 
         if _params.get('source_id') is not None:  # noqa: E501
             _query_params.append(('sourceId', _params['source_id']))
-            _collection_formats['sourceId'] = 'csv'
+            _collection_formats['sourceId'] = 'multi'
 
         if _params.get('event_name') is not None:  # noqa: E501
             _query_params.append(('eventName', _params['event_name']))
-            _collection_formats['eventName'] = 'csv'
+            _collection_formats['eventName'] = 'multi'
 
         if _params.get('event_type') is not None:  # noqa: E501
             _query_params.append(('eventType', _params['event_type']))
-            _collection_formats['eventType'] = 'csv'
+            _collection_formats['eventType'] = 'multi'
 
         if _params.get('app_version') is not None:  # noqa: E501
             _query_params.append(('appVersion', _params['app_version']))
-            _collection_formats['appVersion'] = 'csv'
+            _collection_formats['appVersion'] = 'multi'
 
         if _params.get('pagination') is not None:  # noqa: E501
             _query_params.append(('pagination', _params['pagination']))
