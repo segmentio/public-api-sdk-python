ca5489734f07663f5ba56ab3d590060e57459ee4
APIs and Common Services team <team-apics@twilio.com>
2024-05-28 23:39:00 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@549b9325.

--- segment_public_api/api/reverse_etl_api.py
+++ segment_public_api/api/reverse_etl_api.py
@@ -60,7 +60,7 @@ class ReverseETLApi:
     def create_reverse_etl_manual_sync(self, create_reverse_etl_manual_sync_input : CreateReverseETLManualSyncInput, **kwargs) -> CreateReverseETLManualSync200Response:  # noqa: E501
         """Create Reverse ETL Manual Sync  # noqa: E501
 
-        Triggers a manual sync for a Reverse ETL Connection.   The rate limit for this endpoint is 20 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information.  # noqa: E501
+        Triggers a manual sync for a Reverse ETL Connection.   In the request body, the `subscriptionId` is the id that follows after `/mappings/` portion in the URL of the sync.   For example, the `subscriptionId` would be `2` for this sync: https://app.Segment.com/example-workspace/reverse-etl/destinations/example-destination/sources/example-source/instances/1/mappings/2/source-id/3/model-id/4/sync-details   The rate limit for this endpoint is 20 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information.  # noqa: E501
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
 
@@ -90,7 +90,7 @@ class ReverseETLApi:
     def create_reverse_etl_manual_sync_with_http_info(self, create_reverse_etl_manual_sync_input : CreateReverseETLManualSyncInput, **kwargs) -> ApiResponse:  # noqa: E501
         """Create Reverse ETL Manual Sync  # noqa: E501
 
-        Triggers a manual sync for a Reverse ETL Connection.   The rate limit for this endpoint is 20 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information.  # noqa: E501
+        Triggers a manual sync for a Reverse ETL Connection.   In the request body, the `subscriptionId` is the id that follows after `/mappings/` portion in the URL of the sync.   For example, the `subscriptionId` would be `2` for this sync: https://app.Segment.com/example-workspace/reverse-etl/destinations/example-destination/sources/example-source/instances/1/mappings/2/source-id/3/model-id/4/sync-details   The rate limit for this endpoint is 20 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information.  # noqa: E501
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
 
@@ -646,7 +646,7 @@ class ReverseETLApi:
     def get_reverse_etl_sync_status(self, model_id : constr(strict=True), sync_id : constr(strict=True), **kwargs) -> GetReverseETLSyncStatus200Response:  # noqa: E501
         """Get Reverse ETL Sync Status  # noqa: E501
 
-        Get the sync status for a Reverse ETL sync. The sync status includes all detailed information about the sync, like sync status, duration, details about the extract and load phase if applicable, etc...   The rate limit for this endpoint is 250 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information.  # noqa: E501
+        Get the sync status for a Reverse ETL sync.  The sync status includes all detailed information about the sync - sync status, duration, details about the extract and load phase if applicable, etc.   The rate limit for this endpoint is 250 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information.  # noqa: E501
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
 
@@ -678,7 +678,7 @@ class ReverseETLApi:
     def get_reverse_etl_sync_status_with_http_info(self, model_id : constr(strict=True), sync_id : constr(strict=True), **kwargs) -> ApiResponse:  # noqa: E501
         """Get Reverse ETL Sync Status  # noqa: E501
 
-        Get the sync status for a Reverse ETL sync. The sync status includes all detailed information about the sync, like sync status, duration, details about the extract and load phase if applicable, etc...   The rate limit for this endpoint is 250 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information.  # noqa: E501
+        Get the sync status for a Reverse ETL sync.  The sync status includes all detailed information about the sync - sync status, duration, details about the extract and load phase if applicable, etc.   The rate limit for this endpoint is 250 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information.  # noqa: E501
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
 
