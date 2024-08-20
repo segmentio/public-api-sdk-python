885d721bd9c37b2e7c5372eef90049a3d45166d9
APIs and Common Services team <team-apics@twilio.com>
2024-08-20 17:33:57 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@1881387e.

--- segment_public_api/models/config.py
+++ segment_public_api/models/config.py
@@ -14,59 +14,128 @@
 
 
 from __future__ import annotations
+from inspect import getfullargspec
+import json
 import pprint
 import re  # noqa: F401
-import json
 
+from typing import Optional
+from pydantic import BaseModel, Field, StrictStr, ValidationError, validator
+from segment_public_api.models.reverse_etl_periodic_schedule_config import ReverseEtlPeriodicScheduleConfig
+from segment_public_api.models.reverse_etl_specific_time_schedule_config import ReverseEtlSpecificTimeScheduleConfig
+from typing import Union, Any, List, TYPE_CHECKING
+from pydantic import StrictStr, Field
 
-
-from pydantic import BaseModel, Field, StrictStr
+CONFIG_ANY_OF_SCHEMAS = ["ReverseEtlPeriodicScheduleConfig", "ReverseEtlSpecificTimeScheduleConfig"]
 
 class Config(BaseModel):
     """
-    Config contains interval duration in case of periodic or day and hours in case of specific_days. Empty if strategy is MANUAL.  # noqa: E501
+    Config contains interval duration in case of periodic or day and hours in case of specific_days. Empty if strategy is MANUAL.
     """
-    interval: StrictStr = Field(..., description="Duration is specified as a string, eg: 15m, 3h25m30s.")
-    __properties = ["interval"]
+
+    # data type: ReverseEtlPeriodicScheduleConfig
+    anyof_schema_1_validator: Optional[ReverseEtlPeriodicScheduleConfig] = None
+    # data type: ReverseEtlSpecificTimeScheduleConfig
+    anyof_schema_2_validator: Optional[ReverseEtlSpecificTimeScheduleConfig] = None
+    if TYPE_CHECKING:
+        actual_instance: Union[ReverseEtlPeriodicScheduleConfig, ReverseEtlSpecificTimeScheduleConfig]
+    else:
+        actual_instance: Any
+    any_of_schemas: List[str] = Field(CONFIG_ANY_OF_SCHEMAS, const=True)
 
     class Config:
-        """Pydantic configuration"""
-        allow_population_by_field_name = True
         validate_assignment = True
 
-    def to_str(self) -> str:
-        """Returns the string representation of the model using alias"""
-        return pprint.pformat(self.dict(by_alias=True))
+    def __init__(self, *args, **kwargs) -> None:
+        if args:
+            if len(args) > 1:
+                raise ValueError("If a position argument is used, only 1 is allowed to set `actual_instance`")
+            if kwargs:
+                raise ValueError("If a position argument is used, keyword arguments cannot be used.")
+            super().__init__(actual_instance=args[0])
+        else:
+            super().__init__(**kwargs)
 
-    def to_json(self) -> str:
-        """Returns the JSON representation of the model using alias"""
-        return json.dumps(self.to_dict())
+    @validator('actual_instance')
+    def actual_instance_must_validate_anyof(cls, v):
+        if v is None:
+            return v
 
-    @classmethod
-    def from_json(cls, json_str: str) -> Config:
-        """Create an instance of Config from a JSON string"""
-        return cls.from_dict(json.loads(json_str))
+        instance = Config.construct()
+        error_messages = []
+        # validate data type: ReverseEtlPeriodicScheduleConfig
+        if not isinstance(v, ReverseEtlPeriodicScheduleConfig):
+            error_messages.append(f"Error! Input type `{type(v)}` is not `ReverseEtlPeriodicScheduleConfig`")
+        else:
+            return v
 
-    def to_dict(self):
-        """Returns the dictionary representation of the model using alias"""
-        _dict = self.dict(by_alias=True,
-                          exclude={
-                          },
-                          exclude_none=True)
-        return _dict
+        # validate data type: ReverseEtlSpecificTimeScheduleConfig
+        if not isinstance(v, ReverseEtlSpecificTimeScheduleConfig):
+            error_messages.append(f"Error! Input type `{type(v)}` is not `ReverseEtlSpecificTimeScheduleConfig`")
+        else:
+            return v
+
+        if error_messages:
+            # no match
+            raise ValueError("No match found when setting the actual_instance in Config with anyOf schemas: ReverseEtlPeriodicScheduleConfig, ReverseEtlSpecificTimeScheduleConfig. Details: " + ", ".join(error_messages))
+        else:
+            return v
 
     @classmethod
     def from_dict(cls, obj: dict) -> Config:
-        """Create an instance of Config from a dict"""
-        if obj is None:
-            return None
+        return cls.from_json(json.dumps(obj))
 
-        if not isinstance(obj, dict):
-            return Config.parse_obj(obj)
+    @classmethod
+    def from_json(cls, json_str: str) -> Config:
+        """Returns the object represented by the json string"""
+        instance = Config.construct()
+        if json_str is None:
+            return instance
 
-        _obj = Config.parse_obj({
-            "interval": obj.get("interval")
-        })
-        return _obj
+        error_messages = []
+        # anyof_schema_1_validator: Optional[ReverseEtlPeriodicScheduleConfig] = None
+        try:
+            instance.actual_instance = ReverseEtlPeriodicScheduleConfig.from_json(json_str)
+            return instance
+        except (ValidationError, ValueError) as e:
+             error_messages.append(str(e))
+        # anyof_schema_2_validator: Optional[ReverseEtlSpecificTimeScheduleConfig] = None
+        try:
+            instance.actual_instance = ReverseEtlSpecificTimeScheduleConfig.from_json(json_str)
+            return instance
+        except (ValidationError, ValueError) as e:
+             error_messages.append(str(e))
+
+        if error_messages:
+            # no match
+            raise ValueError("No match found when deserializing the JSON string into Config with anyOf schemas: ReverseEtlPeriodicScheduleConfig, ReverseEtlSpecificTimeScheduleConfig. Details: " + ", ".join(error_messages))
+        else:
+            return instance
+
+    def to_json(self) -> str:
+        """Returns the JSON representation of the actual instance"""
+        if self.actual_instance is None:
+            return "null"
+
+        to_json = getattr(self.actual_instance, "to_json", None)
+        if callable(to_json):
+            return self.actual_instance.to_json()
+        else:
+            return json.dumps(self.actual_instance)
+
+    def to_dict(self) -> dict:
+        """Returns the dict representation of the actual instance"""
+        if self.actual_instance is None:
+            return "null"
+
+        to_json = getattr(self.actual_instance, "to_json", None)
+        if callable(to_json):
+            return self.actual_instance.to_dict()
+        else:
+            return json.dumps(self.actual_instance)
+
+    def to_str(self) -> str:
+        """Returns the string representation of the actual instance"""
+        return pprint.pformat(self.dict())
 
 
