a3ac4de568a9e16440f0e5afe1e6f7393b3ac8c2
APIs and Common Services team <team-apics@twilio.com>
2025-01-16 06:16:50 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@63343aaf.

--- segment_public_api/models/create_source_regulation_v1_input.py
+++ segment_public_api/models/create_source_regulation_v1_input.py
@@ -26,7 +26,7 @@ class CreateSourceRegulationV1Input(BaseModel):
     """
     The input to create a Source-scoped regulation.  # noqa: E501
     """
-    regulation_type: StrictStr = Field(..., alias="regulationType", description="The regulation type to create.")
+    regulation_type: StrictStr = Field(..., alias="regulationType", description="The regulation type to create. Please note that `DELETE_ARCHIVE_ONLY` is only supported for limited Workspaces for Source-scoped regulations.")
     subject_type: StrictStr = Field(..., alias="subjectType", description="The subject type.")
     subject_ids: conlist(StrictStr) = Field(..., alias="subjectIds", description="The list of `userId` or `objectId` values of the subjects to regulate.  Config API note: equal to `parent` but allows an array.")
     __properties = ["regulationType", "subjectType", "subjectIds"]
