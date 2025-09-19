c66401ee9b22565fe769bf247278b7d33432c35e
APIs and Common Services team <team-apics@twilio.com>
2025-09-19 06:18:43 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@190eb1c2.

--- segment_public_api/models/list_function_item_v1.py
+++ segment_public_api/models/list_function_item_v1.py
@@ -42,8 +42,8 @@ class ListFunctionItemV1(BaseModel):
         if value is None:
             return value
 
-        if value not in ('DESTINATION', 'INSERT', 'INSERT_DESTINATION', 'INSERT_SOURCE', 'SOURCE'):
-            raise ValueError("must be one of enum values ('DESTINATION', 'INSERT', 'INSERT_DESTINATION', 'INSERT_SOURCE', 'SOURCE')")
+        if value not in ('DESTINATION', 'INSERT_DESTINATION', 'INSERT_SOURCE', 'INSERT_TRANSFORMATION', 'SOURCE'):
+            raise ValueError("must be one of enum values ('DESTINATION', 'INSERT_DESTINATION', 'INSERT_SOURCE', 'INSERT_TRANSFORMATION', 'SOURCE')")
         return value
 
     class Config:
